b0cce6e3ed1a2deb5e81c8626f20f560082e1127
emilii09 <emiliana.queiroz@escolar.ifrn.edu.br>
2023-09-01 21:57:24 -0300

    terceiro commit

--- core/models.py
+++ core/models.py
@@ -5,9 +5,16 @@ class Stand(models.Model):
     localizacao= models.CharField(max_length=200)
     valor = models.FloatField()
 
+    def __str__(self):
+        return self.localizacao
+
 class Reserva(models.Model):
     cnpj = models.CharField(max_length=200)
     nome_empresa = models.CharField(max_length=200)
     categoria_empresa = models.CharField(max_length=200)
     quitado = models.BooleanField()
     standes = models.ForeignKey(Stand,on_delete=models.CASCADE)
+
+        
+    def __str__(self):
+        return self.localizacao
--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from core.views import index,detalhes,detalhes2
 from reserva.views import reserva_editar,reserva_listar,reserva_remover,reserva_criar
 from stand.views import stand_criar,stand_editar,stand_listar,stand_remover
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name='index'),
@@ -17,4 +19,4 @@ urlpatterns = [
     path('stand/remover/<int:id>/',stand_remover,name='stand_remover'),
     path('stand/editar/<int:id>/',stand_editar,name='stand_editar'),
     path('listar2/',stand_listar,name='stand_listar'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
