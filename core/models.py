309741f6538e57d2ed963ecd844a1bfd8e682851
emiasm <emilianayasmim205@gmail.com>
2023-10-06 16:32:22 -0300

    virgula

--- core/models.py
+++ core/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Stand(models.Model):
-    localizacao= models.CharField(max_length=200)
-    valor = models.FloatField()
+
+    localizacao = models.CharField(
+        verbose_name=("Localização"), max_length=200
+    )
+    valor = models.DecimalField(
+        verbose_name=("Valor"),
+        decimal_places=2,
+        max_digits=6
+    )
 
     def __str__(self):
         return self.localizacao
--- stand/forms.py
+++ stand/forms.py
@@ -1,13 +1,47 @@
-from django import forms 
-from django.forms import ModelForm
+# from django import forms 
+# from django.forms import ModelForm
+# from core.models import Stand
+
+# class StandForm(ModelForm):
+#     class Meta:
+#         model = Stand
+#         fields = "__all__"
+#         widgets = {
+#             'localizacao' : forms.TextInput(attrs={'class': 'form-control' }),
+#             'valor': forms.TextInput(attrs={'class':'form-control'}),
+            
+#         }
+
+
+from decimal import Decimal
+
+from django import forms
+
 from core.models import Stand
 
-class StandForm(ModelForm):
+
+class StandForm(forms.ModelForm):
+
+    localizacao = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Localização do stand",
+        })
+    )
+    valor = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "money",
+            "placeholder": "Valor do stand",
+        })
+    )
+
+    def clean_valor(self):
+        valor = self.cleaned_data["valor"]
+        return Decimal(valor.replace(",", "."))
+
     class Meta:
         model = Stand
-        fields = "__all__"
-        widgets = {
-            'localizacao' : forms.TextInput(attrs={'class': 'form-control' }),
-            'valor': forms.TextInput(attrs={'class':'form-control'}),
-            
-        }
+        fields = (
+            "localizacao",
+            "valor",
+        )
