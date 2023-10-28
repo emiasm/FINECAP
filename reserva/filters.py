040e65811c087573c34b5752bbdd3a29c828541c
emilii09 <emiliana.queiroz@escolar.ifrn.edu.br>
2023-10-28 19:00:16 -0300

    add filtros completo

--- reserva/filters.py
+++ reserva/filters.py
@@ -5,11 +5,11 @@ from core.models import Reserva
 
 class ReservaFilter(django_filters.FilterSet):
     nome_empresa = django_filters.CharFilter(lookup_expr='icontains')
-    cnpj = django_filters.NumberFilter()
+   
 
     class Meta:
         model = Reserva
         fields = [
             'nome_empresa',
-            'cnpj',
+            
         ]
